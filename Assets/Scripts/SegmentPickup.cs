467b7478daae0e26e216ad0c0d6708e2c6942db3
Jeff Stevenson <jwstevenson22@gmail.com>
2024-11-03 17:22:11 -0500

    pickup adjustments

--- Assets/Scripts/SegmentPickup.cs
+++ Assets/Scripts/SegmentPickup.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SegmentPickup : MonoBehaviour
 {
-    [field: SerializeField] public int NumSegments { get; set; }
+    [field: SerializeField] private int minNumberSegments;
+    [field: SerializeField] private int maxNumberSegments;
+    public int NumSegments { get; set; }
+
+    private void Awake()
+    {
+        NumSegments = Random.Range(minNumberSegments, maxNumberSegments);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out SegmentManager segmentManager))
